5d52601f334d0ed098244692d483cd503aa34c8c
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-08-21 12:14:06 -0300

    caso de uso e implementando design patterns

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,24 @@
 import fastify from "fastify";
-import { z } from "zod";
-import { prisma } from "./lib/prisma";
+import { appRoutes } from "@/http/routes";
+import { ZodError } from "zod";
+import { env } from "@/env";
 
 export const app = fastify();
 
-app.post("/users", async (request, reply) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  });
-  const { name, email, password } = registerBodySchema.parse(request.body);
+app.register(appRoutes);
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  });
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() });
+  }
 
-  return reply.status(201).send();
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: "Internal server error." });
 });
