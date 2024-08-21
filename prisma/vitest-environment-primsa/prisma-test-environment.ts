c6dc66c7f5c1ae1aae34c4f445b6caa94f5091d0
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-08-21 18:58:54 -0300

    atualizações ci/cd

--- prisma/vitest-environment-primsa/prisma-test-environment.ts
+++ prisma/vitest-environment-primsa/prisma-test-environment.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config'
-
 import { randomUUID } from 'node:crypto'
 import { execSync } from 'node:child_process'
 import { Environment } from 'vitest'
@@ -13,13 +12,11 @@ function generateDatabaseURL(schema: string) {
   }
 
   const url = new URL(process.env.DATABASE_URL)
-
   url.searchParams.set('schema', schema)
-
   return url.toString()
 }
 
-export default <Environment>{
+const prismaEnvironment = {
   name: 'prisma',
   async setup() {
     const schema = randomUUID()
@@ -34,9 +31,13 @@ export default <Environment>{
         await prisma.$executeRawUnsafe(
           `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
         )
-
         await prisma.$disconnect()
       },
     }
   },
-}
+  transformMode: {
+    ssr: ['ts'],
+  },
+} as unknown as Environment
+
+export default prismaEnvironment
